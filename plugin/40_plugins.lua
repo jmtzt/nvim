5771894de962b1b7317bda58e0c2bc8953608062
Joao Tozato <joao.tozato@voize.de>
2026-01-29 12:49:00 +0100

    disable copilot - testing 10s epoll_pwait hang

--- plugin/40_plugins.lua
+++ plugin/40_plugins.lua
@@ -456,10 +456,11 @@ later(function()
 	end, { desc = "Toggle DAP UI" })
 end)
 
-later(function()
-	add("github/copilot.vim")
-	vim.api.nvim_set_keymap("i", "<C-l>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-end)
+-- DISABLED: Testing if Copilot network connection causes 10s hang on SSH
+-- later(function()
+-- 	add("github/copilot.vim")
+-- 	vim.api.nvim_set_keymap("i", "<C-l>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+-- end)
 
 MiniDeps.now(function()
 	add("folke/snacks.nvim")
